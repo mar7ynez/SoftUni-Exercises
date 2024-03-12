94485b95197cfb6bd9e520e0750f1ec323cc9070
unknown <sadface_@Abv.bg>
2024-03-13 01:37:53 +0200

    Initial solution added

---
+++ Programming Fundamentals with JavaScript - 2024/Regular Expressions - More Exercise/04-santasSecretHelper.js
@@ -0,0 +1,27 @@
+function santasSecretHelper(data) {
+
+    let encryptionKey = data.shift();
+    let message = data.shift();
+    let pattern = /@([A-Z][a-z]+)[^@\-!:>]*!([GN])!/g;
+    let decryptedMessage = [];
+
+    while (message !== 'end') {
+        for (let curChar of message) {
+            decryptedMessage.push(String.fromCharCode(curChar.charCodeAt() - encryptionKey));
+
+        }
+        let match = pattern.exec(decryptedMessage.join(''));
+        
+        if (match && match[2] !== 'N') {
+            console.log(match[1]);
+
+        }
+        message = data.shift();
+    }
+}
+santasSecretHelper(['3',
+'N}eideidmk$"(mnyenmCNlpamnin$J$',
+'ddddkkkkmvkvmCFrqqru-nvevek$J$nmgievnge',
+'ppqmkkkmnolmnnCEhq/vkievk$Q$',
+'yyegiivoguCYdohqwlqh/kguimhk$J$',
+'end']);
