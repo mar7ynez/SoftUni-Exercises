a16b92824217ce51cd7b3e604f2c7444dace3f33
unknown <sadface_@Abv.bg>
2024-03-17 23:33:26 +0200

    Added solution to the first problem of the final exam example-5

---
+++ Programming Fundamentals with JavaScript - 2024/05. Programming Fundamentals Final Exam/01-activationKeys.js
@@ -0,0 +1,60 @@
+function activationKeys(data) {
+
+    let rawKey = data.shift();
+    let command = data.shift();
+
+    while (command !== 'Generate') {
+        if (command.includes('Contains')) {
+            let [, substr] = command.split('>>>');
+            let foundSubstrOrNot = contains(rawKey, substr);
+            console.log(foundSubstrOrNot);
+
+        } else if (command.includes('Flip')) {
+            let [, secAction, startIdx, endIdx] = command.split('>>>');
+            rawKey = flip(secAction, rawKey, Number(startIdx), Number(endIdx));
+            console.log(rawKey);
+
+        } else {
+            let [, startIdx, endIdx] = command.split('>>>');
+            rawKey = partSlice(rawKey, Number(startIdx), Number(endIdx));
+            console.log(rawKey);
+        }
+        command = data.shift();
+    }
+    console.log(`Your activation key is: ${rawKey}`);
+    console.log(`Your activation key is: 134SF5ftuni2020rockz42`);
+
+    function contains(rawKey, substr) {
+        if (rawKey.includes(substr)) {
+            return `${rawKey} contains ${substr}`;
+        }
+        return 'Substring not found!';
+    }
+
+    function flip(action, rawKey, startIdx, endIdx) {
+        if (action === 'Upper') {
+            let substrPart = rawKey.substring(startIdx, endIdx);
+            rawKey = rawKey.replace(substrPart, substrPart.toUpperCase());
+            return rawKey;
+        }
+        let substrPart = rawKey.substring(startIdx, endIdx);
+        rawKey = rawKey.replace(substrPart, substrPart.toLowerCase());
+        return rawKey;
+    }
+
+    function partSlice(rawKey, startIdx, endIdx) {
+        let keyArr = rawKey.split('');
+        keyArr.splice(startIdx, endIdx - startIdx);
+        rawKey = keyArr.join('');
+        return rawKey
+    }
+
+}
+activationKeys((["134softsf5ftuni2020rockz42",
+"Slice>>>3>>>7",
+"Contains>>>-rock",
+"Contains>>>-uni-",
+"Contains>>>-rocks",
+"Flip>>>Upper>>>2>>>8",
+"Flip>>>Lower>>>5>>>11",
+"Generate"]));
