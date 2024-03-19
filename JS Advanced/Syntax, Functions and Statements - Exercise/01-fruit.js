6980e38deb5847a6c7255311f683edd553739024
unknown <sadface_@Abv.bg>
2024-03-20 00:10:41 +0200

    Initial solution

---
+++ JS Advanced/Syntax, Functions and Statements - Exercise/01-fruit.js
@@ -0,0 +1,8 @@
+function fruit(fruitName, weightG, pricePerKg) {
+
+    let gramsToKilos = weightG / 1000;
+    let totalCost = gramsToKilos * pricePerKg;
+
+    console.log(`I need ${totalCost.toFixed(2)} to buy ${gramsToKilos.toFixed(2)} kilograms ${fruitName}.`);
+}
+fruit('apple', 1563, 2.35);
