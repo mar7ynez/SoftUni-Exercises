3facf3527d39d3d59f124bd91ee048d74d25f57c
unknown <sadface_@Abv.bg>
2024-03-22 04:52:16 +0200

    Added solution to this problem

--- Programming Fundamentals with JavaScript - 2024/Arrays Advanced - Lab/04-lastKNumbersSequance.js
+++ Programming Fundamentals with JavaScript - 2024/Arrays Advanced - Lab/04-lastKNumbersSequance.js
@@ -1,6 +1,17 @@
-function lastKNumSequence(firstInt, secondInt) {
+function lastKNumbersSequence(n, k) {
 
-    
+    let result = [1];
 
+    for (let i = 1; i < n; i++) {
+        let sum = 0;
+        let startIdx = result.length < k ? 0 : result.length - k;
+
+        for (let j = startIdx; j < result.length; j++) {
+            sum += result[j];
+
+        }
+        result[i] = sum;
+    }
+    console.log(result.join(' '));;
 }
-lastKNumSequence(6, 3);
+lastKNumbersSequence(6, 3); 
