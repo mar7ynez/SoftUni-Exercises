a56796501c0c5cf43be13be84e084e5c3020d5c5
unknown <sadface_@Abv.bg>
2024-03-26 17:38:36 +0200

    Initial solution

---
+++ JS Advanced/Objects and Composition - Exercise/10-heroes.js
@@ -0,0 +1,43 @@
+function solve() {
+
+    const mage = (name) => {
+        let state = {
+            name,
+            health: 100,
+            mana: 100,
+        }
+        return Object.assign(state, cast(state));
+    };
+    const fighter = (name) => {
+        let state = {
+            name,
+            health: 100,
+            stamina: 100,
+        }
+        return Object.assign(state, fight(state));
+    };
+    const cast = (state) => ({
+        cast: (spell) => {
+            console.log(`${state.name} cast ${spell}`);
+            state.mana--;
+        }
+    });
+    const fight = (state) => ({
+        fight: () => {
+            console.log(`${state.name} slashes at the foe!`);
+            state.stamina--;
+        }
+    });
+    return { mage: mage, fighter: fighter };
+}
+let create = solve();
+const scorcher = create.mage("Scorcher");
+scorcher.cast("fireball")
+scorcher.cast("thunder")
+scorcher.cast("light")
+
+const scorcher2 = create.fighter("Scorcher 2");
+scorcher2.fight()
+
+console.log(scorcher2.stamina);
+console.log(scorcher.mana);
