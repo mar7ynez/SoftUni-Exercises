3c4b900945abf38c57a6fbecd1744aafbde82576
unknown <sadface_@Abv.bg>
2024-03-27 23:26:51 +0200

    Added solution

--- Programming Fundamentals with JavaScript - 2024/Functions - More Exercise/03-pointsValidation.js
+++ Programming Fundamentals with JavaScript - 2024/Functions - More Exercise/03-pointsValidation.js
@@ -1,4 +1,22 @@
 function pointsValidation(arr) {
+    const x1 = arr[0];
+    const y1 = arr[1];
+    const x2 = arr[2];
+    const y2 = arr[3];
 
+    const distanceBetween = (x1, y1, x2, y2) => {
+        let resultNum = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+
+        if (Number.isInteger(resultNum)) {
+            return 'is valid';
+
+        } else {
+            return 'is invalid';
+            
+        }
+    }
+    console.log(`{${x1}, ${y1}} to {0, 0} ${distanceBetween(x1, y1, 0, 0)}`);
+    console.log(`{${x2}, ${y2}} to {0, 0} ${distanceBetween(x2, y2, 0, 0)}`);
+    console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} ${distanceBetween(x1, y1, x2, y2)}`);
 }
 pointsValidation([3, 0, 0, 4]);
