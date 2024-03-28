2e2106f322017e4993c2650170c6f3a809504877
unknown <sadface_@Abv.bg>
2024-03-28 05:19:07 +0200

    Initial solution

---
+++ JS Advanced/DOM Introduction - Lab/04.Show More/ShowMore.js
@@ -0,0 +1,8 @@
+function showText() {
+    
+    let showMoreElement = document.getElementById('text');
+    let readMoreElement = document.getElementById('more');
+
+    showMoreElement.style.display = 'inline';
+    readMoreElement.style.display = 'none';
+}
