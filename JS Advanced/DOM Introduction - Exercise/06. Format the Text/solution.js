f36ff4696ac424e79c5383738fa2b30c9ec6bb3f
unknown <sadface_@Abv.bg>
2024-03-30 00:11:31 +0200

    If-else condition was removed.

--- JS Advanced/DOM Introduction - Exercise/06. Format the Text/solution.js
+++ JS Advanced/DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -5,17 +5,11 @@ function solve() {
 
   let pattern = /\S[\w,\/\s\(\)-]+./g;
   let splitedSentences = input.value.match(pattern);
-  
+
   divElement.innerHTML = '';
 
-  if (splitedSentences.length <= 3) {
-    divElement.innerHTML += `<p>${input.value}</p>`
-
-  } else {
-
-    for (let i = 0; i < splitedSentences.length; i += 3) {
-      let paragraph = splitedSentences.slice(i, 3 + i);
-      divElement.innerHTML += `<p>${paragraph}</p>`;
-    }
+  for (let i = 0; i < splitedSentences.length; i += 3) {
+    let paragraph = splitedSentences.slice(i, 3 + i);
+    divElement.innerHTML += `<p>${paragraph}</p>`;
   }
 }
