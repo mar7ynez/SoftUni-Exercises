82e00708034f3f9a298d3d2894455558d7a03ce2
unknown <sadface_@Abv.bg>
2024-05-14 04:45:45 +0300

    Scope bug was found and fixed, in the last 'else' condition.

--- JS Advanced/DOM Manipulation and Events - Exercise/09. JavaScript-Quiz/solution.js
+++ JS Advanced/DOM Manipulation and Events - Exercise/09. JavaScript-Quiz/solution.js
@@ -24,8 +24,8 @@ function solve() {
     } else {
       sectionElements[2].style.display = 'none';
       e.target.textContent === 'A programming API for HTML and XML documents' ? rightAnswers++ : rightAnswers;
+      resultElement.parentNode.parentNode.style.display = 'block';
+      rightAnswers === 3 ? resultElement.textContent = 'You are recognized as top JavaScript fan!' : resultElement.textContent = `You have ${rightAnswers} right answers`;
     }
-    resultElement.parentNode.parentNode.style.display = 'block';
-    rightAnswers === 3 ? resultElement.textContent = 'You are recognized as top JavaScript fan!' : resultElement.textContent = `You have ${rightAnswers} right answers`;
   }
 }
