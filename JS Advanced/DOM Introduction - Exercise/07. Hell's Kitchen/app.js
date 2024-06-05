2937ac9532af6a79c528edee2bb7fb3e92700d7f
unknown <sadface_@Abv.bg>
2024-06-05 19:17:28 +0300

    Optimized solution

--- JS Advanced/DOM Introduction - Exercise/07. Hell's Kitchen/app.js
+++ JS Advanced/DOM Introduction - Exercise/07. Hell's Kitchen/app.js
@@ -1,51 +1,54 @@
 function solve() {
-
    document.querySelector('#btnSend').addEventListener('click', onClick);
-   let input = document.querySelector('#inputs > textarea');
-   let outputs = document.querySelector('#bestRestaurant > p');
-   let bestWorkers = document.querySelector('#workers > p');
-   
-   function onClick() {
-      let restaurantsList = JSON.parse(input.value);
-      let restaurantsReg = {};
-      let bestRestaurant = [];
-      
-      for (let restaurant of restaurantsList) {
-         let [restName, workersInfo] = restaurant.split(' - ');
-         let workers = workersInfo.split(', ');
-         let totalSalary = [];
-         
-         for (let worker of workers) {
-            let [, workerSalary] = worker.split(' ');
-            workerSalary = Number(workerSalary);
-            
-            if (!restaurantsReg.hasOwnProperty(restName)) {
-               restaurantsReg[restName] = {
-                  name: restName,
-                  workerNames: [],
-                  avgSalary: 0,
-                  bestSalary: 0,
-               }
-            }
-            restaurantsReg[restName].workerNames.push(worker);
-            totalSalary.push(workerSalary);
-         }
-         restaurantsReg[restName].avgSalary = (totalSalary.reduce((a, b) => a + b, 0) / restaurantsReg[restName].workerNames.length).toFixed(2);
-         restaurantsReg[restName].bestSalary = (Math.max(...totalSalary)).toFixed(2);
-         bestRestaurant.push(restaurantsReg[restName]);
-      }
-      bestRestaurant.sort((a, b) => b.avgSalary - a.avgSalary);
-      bestWorkers.textContent = '';
-      let namesAndSalarys = {};
 
-      for (let workerSalary of bestRestaurant[0].workerNames) {
-         let [worker, salary] = workerSalary.split(' ');
-         namesAndSalarys[salary] = worker;
+   function onClick() {
+      let textAreaElement = document.querySelector('#inputs>textarea');
+      let bestRestaurantArea = document.querySelector('#outputs>#bestRestaurant>p');
+      let bestWorkersArea = document.querySelector('#outputs>#workers>p');
+      let restaurants = JSON.parse(textAreaElement.value);
+
+      let restaurantReg = { bestRestaurant: '', bestSal: 0, avgSalary: 0 };
+      let bestWorkersReg = {};
+
+      for (let curRestaurant of restaurants) {
+         let [restaurantName, workerDetails] = curRestaurant.split(' - ');
+         let workersInfo = workerDetails.split(', ');
+
+         if (!bestWorkersReg.hasOwnProperty(restaurantName)) {
+            bestWorkersReg[restaurantName] = {};
+         }
+
+         for (let worker of workersInfo) {
+            let [workerName, workerSalary] = worker.split(' ');
+
+            if (bestWorkersReg.hasOwnProperty(restaurantName)) {
+               bestWorkersReg[restaurantName][workerName] += Number(workerSalary);
+            }
+            bestWorkersReg[restaurantName][workerName] = Number(workerSalary);
+         }
       }
-      
-      for (let key of Object.keys(namesAndSalarys).sort((a, b) => b - a)) {
-         bestWorkers.textContent += ` Name: ${namesAndSalarys[key]} With Salary: ${key}`;
+
+      for (let restaurant in bestWorkersReg) {
+         let bestSalary = [];
+         let curRestSalaries = 0;
+
+         for (let name in bestWorkersReg[restaurant]) {
+            curRestSalaries += Number(bestWorkersReg[restaurant][name]);
+            bestSalary.push(bestWorkersReg[restaurant][name]);
+         }
+
+         if (restaurantReg.avgSalary < curRestSalaries / Object.values(bestWorkersReg[restaurant]).length) {
+            restaurantReg.bestRestaurant = restaurant;
+            restaurantReg.avgSalary = curRestSalaries / Object.values(bestWorkersReg[restaurant]).length;
+            restaurantReg.bestSal = Math.max(...bestSalary);
+         }
       }
-      outputs.textContent = `Name: ${bestRestaurant[0].name} Average Salary: ${bestRestaurant[0].avgSalary} Best Salary: ${bestRestaurant[0].bestSalary}`;
+      bestRestaurantArea.textContent = `Name: ${restaurantReg.bestRestaurant} Average Salary: ${restaurantReg.avgSalary.toFixed(2)} Best Salary: ${restaurantReg.bestSal.toFixed(2)}`;
+
+      let bestEntries = Object.entries(bestWorkersReg[restaurantReg.bestRestaurant]).sort((a, b) => b[1] - a[1]);
+
+      for (let [name, salary] of bestEntries) {
+         bestWorkersArea.textContent += `Name: ${name} With Salary: ${salary} `;
+      }
    }
 }
