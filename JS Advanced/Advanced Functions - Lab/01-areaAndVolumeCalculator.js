b3f1da6eb22f592e40bc114e39687bc2201f47fa
unknown <sadface_@Abv.bg>
2024-06-06 12:10:19 +0300

    Initial solution

--- JS Advanced/Advanced Functions - Lab/01-areaAndVolumeCalculator.js
+++ JS Advanced/Advanced Functions - Lab/01-areaAndVolumeCalculator.js
@@ -10,19 +10,16 @@ function solve(area, vol, input) {
     let coordinates = JSON.parse(input);
     let result = [];
 
-    for (let coordPts of coordinates) {
+    for (let points of coordinates) {
         result.push({
-            area: area.call(coordPts),
-            volume: vol.call(coordPts)
+            area: area.call(points),
+            volume: vol.call(points)
         });
     }
     return result;
 }
-
 solve(area, vol, `[
-    {"x":"10","y":"-22","z":"10"},
-    {"x":"47","y":"7","z":"-5"},
-    {"x":"55","y":"8","z":"0"},
-    {"x":"100","y":"100","z":"100"},
-    {"x":"55","y":"80","z":"250"}
+    {"x":"1","y":"2","z":"10"},
+    {"x":"7","y":"7","z":"10"},
+    {"x":"5","y":"2","z":"10"}
     ]`);
