ddd1361ac59f97085351d29e57e514c004c15f68
unknown <sadface_@abv.bg>
2024-06-28 05:56:21 +0300

    Error 404 handled

--- JS Applications/Asynchronous Programming - Lab/02.Github-Repos/scripts/app.js
+++ JS Applications/Asynchronous Programming - Lab/02.Github-Repos/scripts/app.js
@@ -1,18 +1,29 @@
 function loadRepos() {
 	const usernameInputElement = document.getElementById('username');
 	const reposUlElement = document.getElementById('repos');
+	reposUlElement.innerHTML = '';
 
 	const url = `https://api.github.com/users/${usernameInputElement.value}/repos`;
-
+	
 	fetch(url)
-	.then(response => response.json())
+	.then(response => {
+		if (!response.ok) {
+			throw Error('404 Not Found');
+		}
+		
+		return response.json();
+	})
 	.then(data => {
 		createLiRepo(data);
 	})
+	.catch(error => {
+		const errorLiElement = document.createElement('li');
+		errorLiElement.textContent = error.message;
+
+		reposUlElement.appendChild(errorLiElement);
+	});
 
 	function createLiRepo(data) {
-		reposUlElement.innerHTML = '';
-		
 		data.forEach(repo => {
 			const liElement = document.createElement('li');
 
