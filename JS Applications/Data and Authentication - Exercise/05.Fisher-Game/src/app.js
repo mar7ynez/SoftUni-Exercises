0c96b42575639937ff41b23b8d2f3eae202f50c0
unknown <sadface_@abv.bg>
2024-07-05 16:46:03 +0300

    Initial

---
+++ JS Applications/Data and Authentication - Exercise/05.Fisher-Game/src/app.js
@@ -0,0 +1,27 @@
+const logoutButton = document.querySelector('#logout');
+
+logoutButton.addEventListener('click', onLogout);
+
+function onLogout(e) {
+    const url = 'http://localhost:3030/users/logout';
+    const data = JSON.parse(localStorage.getItem('userData'));
+
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'X-Authorization': data.accessToken
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        
+        localStorage.clear();
+
+        return response.json();
+    })
+    .catch(error => {
+        alert(error.message);
+    });
+}
--- JS Applications/Data and Authentication - Exercise/05.Fisher-Game/src/register.js
+++ JS Applications/Data and Authentication - Exercise/05.Fisher-Game/src/register.js
@@ -1,4 +1,6 @@
-document.querySelector('form').addEventListener('submit', onRegister);
+const formElement = document.querySelector('form');
+
+formElement.addEventListener('submit', onRegister);
 
 const regURL = 'http://localhost:3030/users/register';
 
@@ -8,6 +10,14 @@ function onRegister(e) {
     const formData = new FormData(e.currentTarget);
     const email = formData.get('email');
     const password = formData.get('password');
+    const rePassword = formData.get('rePass');
+
+    if (!email || !password) {
+        return alert('All fields are required!');
+    }
+    if (password !== rePassword) {
+        return alert('Password does not match!')
+    }
 
     fetch(regURL, {
         method: 'POST',
@@ -18,12 +28,17 @@ function onRegister(e) {
     })
     .then(response => {
         if(!response.ok) {
-            throw new Error('All fields required!');
+            throw new Error(response.status);
         }
 
         return response.json();
     })
     .then(data => {
-        console.log(data);
+        localStorage.setItem('userData', JSON.stringify(data));
+        window.location = 'index.html';
+    })
+    .catch(error => {
+        alert(`${error}`);
+        formElement.reset();
     })
 }
