7d8dcd36b9ed715300d742b308e51e62a83ab377
unknown <sadface_@abv.bg>
2024-07-06 15:34:03 +0300

    Handled 'logout' and 'load button' logic

--- JS Applications/Data and Authentication - Exercise/05.Fisher-Game/src/app.js
+++ JS Applications/Data and Authentication - Exercise/05.Fisher-Game/src/app.js
@@ -1,25 +1,32 @@
-const logoutButton = document.querySelector('#logout');
+import { updateNav, getRequest, addHandler, renderCatches,fieldSetRef } from "./util.js";
 
-logoutButton.addEventListener('click', onLogout);
+fieldSetRef.style.border = 'none';
+fieldSetRef.textContent = 'Click to load catches'
+
+updateNav();
+
+addHandler('#logout', 'click', onLogout);
+addHandler('.load', 'click', onLoad);
 
 function onLogout(e) {
     const url = 'http://localhost:3030/users/logout';
+
+    e.preventDefault();
+
     const data = JSON.parse(localStorage.getItem('userData'));
 
-    fetch(url, {
+    const options = {
         method: 'GET',
         headers: {
             'X-Authorization': data.accessToken
         }
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
+    }
 
-            localStorage.clear();
-        })
-        .catch(error => {
-            alert(error.message);
-        });
+    getRequest(url, null, options, true, true);
+}
+
+function onLoad(e) {
+    const url = 'http://localhost:3030/data/catches';
+
+    getRequest(url, renderCatches);
 }
