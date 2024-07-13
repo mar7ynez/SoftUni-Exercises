d0df5b60268d08f4f4791a0db54d38fd20f95794
unknown <sadface_@abv.bg>
2024-07-13 23:56:41 +0300

    requester logic added

---
+++ JS Applications/Single Page Applications - Exercise/01.Forum/src/api.js
@@ -0,0 +1,33 @@
+const hostName = 'http://localhost:3030';
+const endpoints = {
+    topics: 'http://localhost:3030/jsonstore/collections/myboard/posts',
+    comments: 'http://localhost:3030/jsonstore/collections/myboard/comments'
+}
+
+function request(method, url, data) {
+    const options = {
+        method,
+        headers: {},
+    }
+
+    if (data) {
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(data);
+    }
+
+    return fetch(`${hostName}${url}`, options)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+
+            return response.json();
+        })
+        .catch(error => {
+            alert(error.message);
+        });
+}
+
+const post = (url, data) => request('POST', url, data);
+
+export { post, endpoints };
