8e8f56246c0a4c46514406738359f9570acaf395
unknown <sadface_@abv.bg>
2024-07-17 16:26:40 +0300

    Component

--- JS Applications/Single Page Applications - Exercise/01.Forum/src/topicComponent.js
+++ JS Applications/Single Page Applications - Exercise/01.Forum/src/topicComponent.js
@@ -1,31 +1,26 @@
-function createComponent(data, topicTitleDiv) {
-    data.forEach(curTopic => {
-        const topicContainer = document.createElement('div');
-        topicContainer.classList.add('topic-container');
+function createComponent(data) {
+    const topicWrapper = document.createElement('div');
+    topicWrapper.classList.add('topic-name-wrapper');
+    topicWrapper.setAttribute('data-id', data._id);
 
-        topicContainer.innerHTML = `
-        <div class="topic-name-wrapper">
-            <div class="topic-name">
-                <a href="#" class="normal">
-                    <h2>${curTopic.topicName}</h2>
-                </a>
-                <div class="columns">
-                    <div>
-                        <p>Date: <time>${curTopic.date}</time></p>
-                        <div class="nick-name">
-                            <p>Username: <span>${curTopic.username}</span></p>
-                        </div>
+    topicWrapper.innerHTML = `
+        <div class="topic-name">
+            <a href="#" class="normal">
+                <h2>${data.topicName}</h2>
+            </a>
+            <div class="columns">
+                <div>
+                    <p>Date: <time>${data.date}</time></p>
+                    <div class="nick-name">
+                        <p>Username: <span>${data.username}</span></p>
                     </div>
-                
-                
                 </div>
+                
+                
             </div>
         </div>
         `;
-        
-        topicTitleDiv.appendChild(topicContainer);
-    });
-
+    return topicWrapper;
 }
 
 export { createComponent };
