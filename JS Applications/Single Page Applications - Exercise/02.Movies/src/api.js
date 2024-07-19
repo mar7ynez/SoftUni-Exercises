49194a42644a04406da4010b65274bbbb8c60a65
unknown <sadface_@abv.bg>
2024-07-19 23:07:20 +0300

    Requester logic added

---
+++ JS Applications/Single Page Applications - Exercise/02.Movies/src/api.js
@@ -0,0 +1,46 @@
+import { getUserData } from './utils.js';
+
+const hostName = 'http://localhost:3030';
+const endpoints = {
+    movies: '/data/movies',
+}
+
+function requester(method, url, data) {
+
+    const options = {
+        method,
+        headers: {},
+    }
+
+    if (data) {
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(data);
+    }
+
+    const userData = getUserData();
+
+    if (userData) {
+        options.headers['X-Authorization'] = userData.accessToken;
+    }
+
+    return fetch(`${hostName}${url}`, options)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+
+            return response.json();
+        })
+        .catch(error => {
+            alert(error.message);
+        });
+}
+
+const api = {
+    get: (url) => requester('GET', url),
+    post: (url, data) => requester('POST', url, data),
+    put: (url, data) => requester('PUT', url, data),
+    delete: (url) => requester('DELETE', url),
+}
+
+export { api, endpoints };
