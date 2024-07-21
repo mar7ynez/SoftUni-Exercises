9f3768f1c7116f701f571621b47a916eaed3e787
unknown <sadface_@abv.bg>
2024-07-21 22:51:20 +0300

    Details event added

--- JS Applications/Single Page Applications - Exercise/02.Movies/src/app.js
+++ JS Applications/Single Page Applications - Exercise/02.Movies/src/app.js
@@ -1,9 +1,11 @@
 import { updateNav } from './nav.js';
+import { getMovieById } from './pages/details.js';
 import { router } from './router.js';
-
-const navigation = document.querySelector('nav');
+import { getUserData } from './utils.js';
 
 updateNav();
+
+const navigation = document.querySelector('nav');
 
 navigation.addEventListener('click', (e) => {
     e.preventDefault();
@@ -12,4 +14,35 @@ navigation.addEventListener('click', (e) => {
         const url = new URL(e.target);
         router(url.pathname);
     }
+})
+
+const addMovieButtonSection = document.querySelector('#add-movie-button');
+
+addMovieButtonSection.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const url = new URL(e.target);
+    router(url.pathname);
+})
+
+const moviesList = document.querySelector('#movies-list');
+
+moviesList.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (e.target.nodeName !== 'BUTTON') {
+        return;
+    }
+
+    const userData = getUserData();
+
+    if (!userData) {
+        return;
+    }
+
+    const movieId = e.target.parentNode.dataset.id;
+    getMovieById(movieId);
+
+    const url = new URL(e.target.parentNode);
+    router(url.pathname);
 })
