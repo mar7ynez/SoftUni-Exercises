347c6a94ec2b333e4006ad342bf03d560514a207
unknown <sadface_@abv.bg>
2024-07-31 15:32:34 +0300

    Routes added

--- JS Applications/Architecture and Testing - Exercise/03.SoftTerest/src/routes.js
+++ JS Applications/Architecture and Testing - Exercise/03.SoftTerest/src/routes.js
@@ -1,24 +1,19 @@
+import { onLogout } from "./logout.js";
+import { renderCreate } from "./pages/create.js";
 import { renderDashboard } from "./pages/dashboard.js";
+import { renderDetails } from "./pages/details.js";
 import { renderHome } from "./pages/home.js";
 import { renderLogin } from "./pages/login.js";
 import { renderRegister } from "./pages/register.js";
-// import { hideContent } from "./utils.js"
 
 const routes = {
     '/': renderHome,
     '/dashboard': renderDashboard,
     '/login': renderLogin,
     '/register': renderRegister,
-    '/create': () => console.log('Create'),
-    '/logout': () => console.log('logout')
+    '/create': renderCreate,
+    '/details': renderDetails,
+    '/logout': onLogout,
 }
-
-// function router(pathname) {
-//     hideContent();
-
-//     const viewRenderer = routes[pathname];
-
-//     viewRenderer();
-// }
 
 export { routes };
