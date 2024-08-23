9a5981118672693559a09d58810351c5c5a8b1ab
unknown <sadface_@abv.bg>
2024-08-23 23:29:40 +0300

    Card 'template' moved in.

--- JS Back-End/Intro to Node.js/01.Cat Shelter Workshop/views/homeView.html.js
+++ JS Back-End/Intro to Node.js/01.Cat Shelter Workshop/views/homeView.html.js
@@ -1,4 +1,14 @@
-const { catTemp } = require('../cats');
+const catTemp = (catInfo) =>
+    `<li>
+        <img src="${catInfo.imageUrl}" alt="Black Cat">
+        <h3>${catInfo.name}</h3>
+        <p><span>Breed: </span>${catInfo.breed}</p>
+        <p><span>Description: </span>${catInfo.description}</p>
+        <ul class="buttons">
+            <li class="btn edit"><a href="">Change Info</a></li>
+            <li class="btn delete"><a href="">New Home</a></li>
+        </ul>
+    </li>`;
 
 const homeHtml = (cats) => `
 <!DOCTYPE html>
