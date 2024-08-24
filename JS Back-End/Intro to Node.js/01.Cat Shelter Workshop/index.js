7f5a39359534fecf22f79904161f4d335a55ce84
unknown <sadface_@abv.bg>
2024-08-24 23:59:19 +0300

    Add logic added.

--- JS Back-End/Intro to Node.js/01.Cat Shelter Workshop/index.js
+++ JS Back-End/Intro to Node.js/01.Cat Shelter Workshop/index.js
@@ -1,9 +1,11 @@
 const http = require('http');
 const homeHtml = require('./views/homeView.html');
+const { parse } = require('querystring');
 const siteCss = require('./views/site.css');
 const addBreed = require('./views/addBreed.html');
 const addCat = require('./views/addCat.html');
 const { getCats } = require('./cats');
+const { saveCat } = require('./cats');
 
 const server = http.createServer((req, res) => {
 
@@ -20,6 +22,29 @@ const server = http.createServer((req, res) => {
             break;
 
         case '/cats/add-cat': res.writeHead(200, { 'Content-Type': 'text/html' }); res.write(addCat);
+            break;
+
+        case '/cats/add':
+            if (req.method === 'POST') {
+                let body = '';
+                req.on('data', chunk => {
+                    body += chunk.toString();
+                });
+                req.on('end', () => {
+                    const formData = parse(body);
+                    console.log(formData);
+
+                    const newCat = {
+                        id: Date.now(),
+                        imageUrl: formData.upload || 'default_image_url.jpg',
+                        name: formData.name,
+                        breed: formData.breed,
+                        description: formData.description
+                    };
+
+                    saveCat(newCat);
+                });
+            }
             break;
 
         default: res.writeHead(404, { 'Content-Type': 'text/html' }); res.write('<h1>404 Not found</h1>');
