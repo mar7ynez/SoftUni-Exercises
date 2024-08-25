cf4fa617cd253f80c93c77953930e10f5014b313
unknown <sadface_@abv.bg>
2024-08-25 23:46:47 +0300

    Refactored the code to work with imageUrl.

--- JS Back-End/Intro to Node.js/01.Cat Shelter Workshop/index.js
+++ JS Back-End/Intro to Node.js/01.Cat Shelter Workshop/index.js
@@ -1,57 +1,79 @@
 const http = require('http');
 const homeHtml = require('./views/homeView.html');
-const { parse } = require('querystring');
 const siteCss = require('./views/site.css');
 const addBreed = require('./views/addBreed.html');
 const addCat = require('./views/addCat.html');
 const { getCats } = require('./cats');
 const { saveCat } = require('./cats');
+const formidable = require('formidable');
 
 const server = http.createServer((req, res) => {
-
     const cats = getCats();
 
     switch (req.url) {
-        case '/': res.writeHead(200, { 'Content-Type': 'text/html' }); res.write(homeHtml(cats));
+        case '/':
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(homeHtml(cats));
+            res.end();
             break;
 
-        case '/styles/site.css': res.writeHead(200, { 'Content-Type': 'text/css' }); res.write(siteCss);
+        case '/styles/site.css':
+            res.writeHead(200, { 'Content-Type': 'text/css' });
+            res.write(siteCss);
+            res.end();
             break;
 
-        case '/cats/add-breed': res.writeHead(200, { 'Content-Type': 'text/html' }); res.write(addBreed);
+        case '/cats/add-breed':
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(addBreed);
+            res.end();
             break;
 
-        case '/cats/add-cat': res.writeHead(200, { 'Content-Type': 'text/html' }); res.write(addCat);
+        case '/cats/add-cat':
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(addCat);
+            res.end();
             break;
 
         case '/cats/add':
             if (req.method === 'POST') {
-                let body = '';
-                req.on('data', chunk => {
-                    body += chunk.toString();
-                });
-                req.on('end', () => {
-                    const formData = parse(body);
-                    console.log(formData);
+                const form = new formidable.IncomingForm(); // Initialize Formidable
+
+                form.parse(req, (err, fields, files) => {
+                    if (err) {
+                        res.writeHead(400, { 'Content-Type': 'text/html' });
+                        res.write('<h1>Error processing the form</h1>');
+                        res.end();
+                        return;
+                    }
+
+                    const name = Array.isArray(fields.name) ? fields.name[0] : fields.name;
+                    const breed = Array.isArray(fields.breed) ? fields.breed[0] : fields.breed;
+                    const description = Array.isArray(fields.description) ? fields.description[0] : fields.description;
+                    const imageUrl = Array.isArray(fields.imageUrl) ? fields.imageUrl[0] : fields.imageUrl;
 
                     const newCat = {
                         id: Date.now(),
-                        imageUrl: formData.upload || 'default_image_url.jpg',
-                        name: formData.name,
-                        breed: formData.breed,
-                        description: formData.description
+                        imageUrl: imageUrl,
+                        name: name,
+                        breed: breed,
+                        description: description
                     };
 
                     saveCat(newCat);
+
+                    res.writeHead(302, { Location: '/' });
+                    res.end();
                 });
             }
             break;
 
-        default: res.writeHead(404, { 'Content-Type': 'text/html' }); res.write('<h1>404 Not found</h1>');
+        default:
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.write('<h1>404 Not Found</h1>');
+            res.end();
             break;
     }
+});
 
-    res.end();
-})
-
-server.listen(5000, () => console.log('Server is running on port 5000..'));
+server.listen(5000, () => console.log('Server is running on port 5000...'));
