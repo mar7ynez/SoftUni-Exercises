cceec19042707e3bc4802a0712b3980241c87023
unknown <sadface_@abv.bg>
2024-08-25 23:47:39 +0300

    Read and Save data.

--- JS Back-End/Intro to Node.js/01.Cat Shelter Workshop/cats.js
+++ JS Back-End/Intro to Node.js/01.Cat Shelter Workshop/cats.js
@@ -3,14 +3,15 @@ const path = require('path');
 
 const dataPath = path.join(__dirname, 'catsData.json');
 
-const getCats = () => {
+function getCats() {
     const data = fs.readFileSync(dataPath);
-
     return JSON.parse(data);
 }
 
-const saveCat = (newCat) => {
-    fs.writeFileSync(dataPath, JSON.stringify(newCat));
+function saveCat(newCat) {
+    let cats = getCats();
+    cats.push(newCat);
+    fs.writeFileSync(dataPath, JSON.stringify(cats, null, 2));
 }
 
 module.exports = {
