a96b3ac34a9b13bd23abe77e30fd3c239799651c
unknown <sadface_@abv.bg>
2024-09-07 20:26:26 +0200

    Config Handlebars

---
+++ JS Back-End/ExpressJS and Templating/Workshop - Movie Magic/src/config/cfgHandlebars.js
@@ -0,0 +1,12 @@
+const handlebars = require('express-handlebars');
+const path = require('path');
+
+const configHandlebars = (app) => {
+    app.engine('hbs', handlebars.engine({
+        extname: 'hbs'
+    }));
+    app.set('view engine', 'hbs');
+    app.set('views', path.join(__dirname, '..', 'views'));
+}
+
+module.exports = configHandlebars;
