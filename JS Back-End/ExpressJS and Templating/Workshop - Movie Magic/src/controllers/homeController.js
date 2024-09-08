6bb83bb1bef702959344f5cb5af6619ebf547f92
unknown <sadface_@abv.bg>
2024-09-08 15:34:04 +0300

    Add controllers

---
+++ JS Back-End/ExpressJS and Templating/Workshop - Movie Magic/src/controllers/homeController.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    res.render('home');
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
+});
+
+router.get('/search', (req, res) => {
+    res.render('search');
+});
+
+router.get('/404', (req, res) => {
+    res.render('404');
+});
+
+module.exports = router;
