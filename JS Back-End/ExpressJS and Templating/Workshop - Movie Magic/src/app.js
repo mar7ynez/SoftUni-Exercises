1dc06bd22d00283913aa5797bdc2c2bb654014b4
unknown <sadface_@abv.bg>
2024-09-08 15:30:34 +0300

    Add app routes

--- JS Back-End/ExpressJS and Templating/Workshop - Movie Magic/src/app.js
+++ JS Back-End/ExpressJS and Templating/Workshop - Movie Magic/src/app.js
@@ -3,10 +3,14 @@ const express = require('express');
 const configExpress = require('./config/cfgExpress');
 const configHandlebars = require('./config/cfgHandlebars');
 
+const routes = require('./routes');
+
 const app = express();
 const port = 5000;
 
 configExpress(app);
 configHandlebars(app);
+
+app.use(routes);
 
 app.listen(port, () => console.log(`The server is running on port ${port}...`));
