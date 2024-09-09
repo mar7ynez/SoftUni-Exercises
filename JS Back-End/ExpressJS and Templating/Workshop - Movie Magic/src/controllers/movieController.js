99ca95470c4aa0bd85ee2a8dbb406c12f3fd39d6
unknown <sadface_@abv.bg>
2024-09-09 23:43:56 +0300

    Handle create and movie details

--- JS Back-End/ExpressJS and Templating/Workshop - Movie Magic/src/controllers/movieController.js
+++ JS Back-End/ExpressJS and Templating/Workshop - Movie Magic/src/controllers/movieController.js
@@ -1,7 +1,25 @@
 const router = require('express').Router();
+const movieServices = require('../services/movieService');
 
 router.get('/create', (req, res) => {
     res.render('create');
+});
+
+router.post('/create', (req, res) => {
+    const newMovie = req.body;
+
+    movieServices.addMovie(newMovie);
+
+    res.redirect('/');
+})
+
+router.get('/movie/details/:movieId', (req, res) => {
+    movieServices.getOne(req.params.movieId)
+        .then(movie => {
+            movie.rating = new Array(Number(movie.rating)).fill(1);
+            
+            res.render('details', movie);
+        })
 });
 
 module.exports = router;
