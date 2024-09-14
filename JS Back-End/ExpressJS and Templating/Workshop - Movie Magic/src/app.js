7a6e45e9147dbc4045f16280bfdbba796d7fd212
unknown <sadface_@abv.bg>
2024-09-14 20:43:01 +0300

    Connect DB

--- JS Back-End/ExpressJS and Templating/Workshop - Movie Magic/src/app.js
+++ JS Back-End/ExpressJS and Templating/Workshop - Movie Magic/src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const configExpress = require('./config/cfgExpress');
 const configHandlebars = require('./config/cfgHandlebars');
 
@@ -13,4 +13,10 @@ configHandlebars(app);
 
 app.use(routes);
 
-app.listen(port, () => console.log(`The server is running on port ${port}...`));
+mongoose.connect('mongodb://localhost:27017/magic-movies')
+    .then(() => {
+        console.log('DB Connected!');
+
+        app.listen(port, () => console.log(`The server is running on port ${port}...`));
+    })
+    .catch(error => console.log(`Cannot connect to DB: ${error.message}`));
