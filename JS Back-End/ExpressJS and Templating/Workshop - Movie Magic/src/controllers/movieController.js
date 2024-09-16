c20b48d1168f27dd28692e40612f96c3286aeb7a
unknown <sadface_@abv.bg>
2024-09-16 23:34:33 +0300

    Refactor movieController to deal trough DB

--- JS Back-End/ExpressJS and Templating/Workshop - Movie Magic/src/controllers/movieController.js
+++ JS Back-End/ExpressJS and Templating/Workshop - Movie Magic/src/controllers/movieController.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const movieServices = require('../services/movieService');
+const castServices = require('../services/castService');
 
 router.get('/create', (req, res) => {
     res.render('create');
@@ -13,16 +14,37 @@ router.post('/create', (req, res) => {
             res.redirect('/');
         })
         .catch(error => {
-            alert(`Cannot create movie: ${error.message}`)
+            alert(`Cannot create movie: ${error.message}`);
         });
-})
+});
 
 router.get('/movie/details/:movieId', (req, res) => {
-    movieServices.getOne(req.params.movieId)
+    movieServices.getOne(req.params.movieId).lean()
         .then(movie => {
             movie.rating = new Array(Number(movie.rating)).fill(1);
 
             res.render('details', movie);
+        })
+        .catch(error => console.log(`Cannot get the moive: ${error.message}`));
+});
+
+router.get('/movie/:movieId/attach', (req, res) => {
+    Promise.all([
+        movieServices.getOne(req.params.movieId).lean(),
+        castServices.getAll().lean()
+    ])
+        .then(([movie, casts]) => {
+            res.render('movie/attach', { ...movie, casts })
+        })
+        .catch(error => console.log(`Cannot get movie or casts: ${error.message}`));
+});
+
+router.post('/movie/:movieId/attach', (req, res) => {
+    movieServices.attach(req.params.movieId, req.body.cast)
+        .then((data) => {
+            console.log(data);
+
+            res.redirect(`/movie/${req.params.movieId}/attach`)
         })
 });
 
