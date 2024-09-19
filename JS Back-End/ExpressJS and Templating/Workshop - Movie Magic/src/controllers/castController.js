0346fadc8a9db53e17d0931bf966120b716030ab
unknown <sadface_@abv.bg>
2024-09-20 01:12:40 +0300

    Remove 'cast' from endpoint

--- JS Back-End/ExpressJS and Templating/Workshop - Movie Magic/src/controllers/castController.js
+++ JS Back-End/ExpressJS and Templating/Workshop - Movie Magic/src/controllers/castController.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const castServices = require('../services/castService');
 
-router.get('/create/cast', (req, res) => {
+router.get('/create', (req, res) => {
     res.render('cast/create');
 });
 
-router.post('/create/cast', (req, res) => {
+router.post('/create', (req, res) => {
     castServices.create(req.body)
         .then(() => {
             res.redirect('/');
