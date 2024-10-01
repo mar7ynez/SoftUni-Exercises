ae0d0357430834eef1a4949e6756f57abfd46bdd
unknown <sadface_@abv.bg>
2024-10-01 22:50:47 +0300

    Handle errors in movie controller

--- JS Back-End/ExpressJS and Templating/Workshop - Movie Magic/src/controllers/movieController.js
+++ JS Back-End/ExpressJS and Templating/Workshop - Movie Magic/src/controllers/movieController.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const movieServices = require('../services/movieService');
 const castServices = require('../services/castService');
 const { isAuth } = require('../middlewares/authMiddleware');
+const getErrorMsg = require('../utils/errorUtils');
 
 router.get('/create', isAuth, (req, res) => {
     res.render('movie/create');
@@ -19,20 +20,26 @@ router.post('/create', (req, res) => {
             res.redirect('/');
         })
         .catch(error => {
-            alert(`Cannot create movie: ${error.message}`);
+            res.render('movie/create', { error: getErrorMsg(error), ...req.body });
         });
 });
 
 router.get('/details/:movieId', (req, res) => {
     movieServices.getOne(req.params.movieId).lean()
         .then(movie => {
+            if (!movie) {
+                throw new Error('Cannot get the movie!');
+            }
+
             const isOwner = req.user?._id == movie._ownerId;
 
             movie.rating = new Array(Number(movie.rating)).fill(1);
 
             res.render('movie/details', { ...movie, isOwner });
         })
-        .catch(error => console.log(`Cannot get the moive: ${error.message}`));
+        .catch(error => {
+            res.render('home/home', { error: getErrorMsg(error) });
+        });
 });
 
 router.get('/:movieId/attach', isAuth, (req, res) => {
@@ -41,40 +48,67 @@ router.get('/:movieId/attach', isAuth, (req, res) => {
         castServices.getAll().lean()
     ])
         .then(([movie, casts]) => {
-            res.render('movie/attach', { ...movie, casts })
+            if (!movie || !casts) {
+                throw new Error('Error getting movie or casts!');
+            }
+
+            res.render('movie/attach', { ...movie, casts });
         })
-        .catch(error => console.log(`Cannot get movie or casts: ${error.message}`));
+        .catch(error => {
+            res.render('movie/attach', { error: getErrorMsg(error) });
+        });
 });
 
 router.post('/:movieId/attach', (req, res) => {
-    movieServices.attach(req.params.movieId, req.body.cast)
+    movieServices.attach(req.params.moviesId, req.body.cast)
         .then(() => {
-            res.redirect(`/movie/${req.params.movieId}/attach`)
+            res.redirect(`/movie/${req.params.movieId}/attach`);
         })
+        .catch(error => {
+            res.render('movie/attach', { error: getErrorMsg(error) });
+        });
 });
 
 router.get('/:movieId/edit', isAuth, (req, res) => {
     movieServices.getOne(req.params.movieId).lean()
         .then(movie => {
+            if (!movie) {
+                throw new Error('Error getting the movie!');
+            }
+
             res.render('movie/edit', movie);
         })
-        .catch(error => console.log(`Cannot get the moive: ${error.message}`));
+        .catch(error => {
+            res.render('movie/details', { error: getErrorMsg(error) });
+        });
 });
 
 router.post('/:movieId/edit', (req, res) => {
     movieServices.edit(req.params.movieId, req.body)
-        .then(() => {
+        .then((movie) => {
+            if (!movie) {
+                throw new Error('Error editing the movie!');
+            }
+
             res.redirect(`/movie/details/${req.params.movieId}`);
         })
-        .catch(error => console.log('Error editing the movie\n', error));
+        .catch(error => {
+            res.render('movie/details', { error: getErrorMsg(error) });
+        });
 });
 
 router.get('/:movieId/delete', (req, res) => {
-    movieServices.del(req.params.movieId)
-        .then(() => {
+    movieServices.del(req.params.movsieId)
+        .then((movie) => {
+            if (!movie) {
+                throw new Error('Error deleting the movie!');
+            }
+
             res.redirect('/');
         })
-        .catch(error => console.log('Error deleting the movie\n', error));
+        .catch(error => {
+            res.render('movie/details', { error: getErrorMsg(error) });
+        });
 });
 
 module.exports = router;
