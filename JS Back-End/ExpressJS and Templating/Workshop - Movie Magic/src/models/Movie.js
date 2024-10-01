ef14a8dd315b918b766a001b07ef66b68fef61ba
unknown <sadface_@abv.bg>
2024-10-01 22:49:40 +0300

    Add validations and error messages

--- JS Back-End/ExpressJS and Templating/Workshop - Movie Magic/src/models/Movie.js
+++ JS Back-End/ExpressJS and Templating/Workshop - Movie Magic/src/models/Movie.js
@@ -3,40 +3,43 @@ const mongoose = require('mongoose');
 const movieSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: true,
-        trim: true
+        required: [true, 'Title is required!'],
+        minLength: [5, 'Title should be atleast 5 characters long!'],
+        match: [/^[\w\s]+$/, 'Title does not match the requirements!']
     },
     genre: {
         type: String,
-        required: true,
+        required: [true, 'Genre is required!'],
         lowercase: true,
-        trim: true
+        match: [/^[\w\s]+$/, 'Genre does not match the requirements!']
     },
     director: {
         type: String,
-        required: true
+        required: [true, 'Director is required!'],
+        match: [/^[\w\s]+$/, 'Director does not match the requirements!']
     },
     year: {
         type: Number,
-        required: true,
-        min: 1950,
-        max: 2030,
+        required: [true, 'Year is required!'],
+        min: [1900, 'The year should be between 1900 and 2024!'],
+        max: [2024, 'The year should be between 1900 and 2024!'],
     },
     rating: {
         type: Number,
-        required: true,
-        min: 1,
-        max: 5
+        required: [true, 'Rating is required!'],
+        min: [1, 'The rating should be 1 or bigger!'],
+        max: [5, 'The rating should be 5 or lower!']
     },
     description: {
         type: String,
-        required: true,
-        maxLength: 1500,
+        required: [true, 'Description is required!'],
+        maxLength: [1500, 'Description max length is 1500 characters!'],
+        minLength: [20, 'Description should be atleast 20 characters long!']
     },
     imageUrl: {
         type: String,
-        required: true,
-        match: /^https?:\/\//
+        required: [true, 'Image url is required!'],
+        match: [/^https?:\/\//, 'URL does not match!']
     },
     casts: [{
         type: mongoose.Types.ObjectId,
