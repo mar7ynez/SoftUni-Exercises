fbe58ba66892eba5076eeebe670fd98c491409c7
unknown <sadface_@abv.bg>
2024-10-02 11:02:54 +0300

    Handle create cast error

--- JS Back-End/ExpressJS and Templating/Workshop - Movie Magic/src/controllers/castController.js
+++ JS Back-End/ExpressJS and Templating/Workshop - Movie Magic/src/controllers/castController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const castServices = require('../services/castService');
 const { isAuth } = require('../middlewares/authMiddleware');
+const getErrorMsg = require('../utils/errorUtils');
 
 router.get('/create', isAuth, (req, res) => {
     res.render('cast/create');
@@ -10,6 +11,9 @@ router.post('/create', (req, res) => {
     castServices.create(req.body)
         .then(() => {
             res.redirect('/');
+        })
+        .catch(error => {
+            res.render('cast/create', { error: getErrorMsg(error), ...req.body });
         });
 })
 module.exports = router;
