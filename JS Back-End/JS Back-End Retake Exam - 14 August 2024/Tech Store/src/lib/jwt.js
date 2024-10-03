2cf5ef274f2ecec854eadcd19511bcb2cc90146c
unknown <sadface_@abv.bg>
2024-10-03 21:47:23 +0300

    Convert JWT to promise

---
+++ JS Back-End/JS Back-End Retake Exam - 14 August 2024/Tech Store/src/lib/jwt.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+
+export const sign = (payload, secretOrPrivateKey, options) => {
+    const promise = new Promise((resolve, reject) => {
+        jwt.sign(payload, secretOrPrivateKey, options, (error, token) => {
+            if (error) {
+                return reject(error);
+            }
+
+            return resolve(token);
+        });
+    });
+
+    return promise;
+}
+
+export const verify = (token, secretOrPublicKey, options) => {
+    const promise = new Promise((resolve, reject) => {
+        jwt.verify(token, secretOrPublicKey, options, (error, decodedToken) => {
+            if (error) {
+                return reject(error);
+            }
+
+            return resolve(decodedToken);
+        });
+    });
+
+    return promise;
+}
