b5f3e153fdfb8b3b2dc444e6d0a3c15a77b4f4ed
unknown <sadface_@abv.bg>
2024-10-03 21:47:45 +0300

    Add auth middleware

---
+++ JS Back-End/JS Back-End Retake Exam - 14 August 2024/Tech Store/src/middlewares/authMiddleware.js
@@ -0,0 +1,30 @@
+import * as jwt from "../lib/jwt.js";
+import "dotenv/config";
+
+export const auth = async (req, res, next) => {
+    const token = req.cookies['auth'];
+
+    if (!token) {
+        return next();
+    }
+
+    try {
+        const decodedToken = await jwt.verify(token, process.env.SECRET_KEY)
+
+        req.user = decodedToken;
+
+        next();
+    }
+    catch (error) {
+        res.clearCookie('auth');
+        res.redirect('/user/login');
+    }
+}
+
+export const isAuth = () => {
+    if (!req.user) {
+        return res.redirect('/user/login');
+    }
+
+    next();
+}
