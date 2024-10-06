6d00459399be477c11e664390a3981aa018bbcb9
unknown <sadface_@abv.bg>
2024-10-06 23:28:53 +0300

    Handle errors in product controller

--- JS Back-End/JS Back-End Retake Exam - 14 August 2024/Tech Store/src/controllers/productController.js
+++ JS Back-End/JS Back-End Retake Exam - 14 August 2024/Tech Store/src/controllers/productController.js
@@ -1,14 +1,16 @@
 import express from "express";
 import * as productService from "../services/productService.js";
 import mongoose from "mongoose";
+import { getErrorMsg } from "../utils/errorUtils.js";
+import { isAuth } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.get('/create', (req, res) => {
+router.get('/create', isAuth, (req, res) => {
     res.render('product/create');
 });
 
-router.post('/create', async (req, res) => {
+router.post('/create', isAuth, async (req, res) => {
     const product = {
         ...req.body,
         owner: req.user._id
@@ -20,7 +22,7 @@ router.post('/create', async (req, res) => {
         res.redirect('/');
     }
     catch (error) {
-        console.log('Create Error\n', error);
+        res.render('product/create', { error: getErrorMsg(error) });
     }
 });
 
@@ -28,12 +30,10 @@ router.get('/catalog', async (req, res) => {
     try {
         const products = await productService.getAll().lean();
 
-        console.log(products)
-
         res.render('product/catalog', { products });
     }
     catch (error) {
-        console.log('Error getting device catalog');
+        res.render('product/catalog', { error: getErrorMsg(error) });
     }
 });
 
@@ -43,21 +43,22 @@ router.get('/:productId/details', async (req, res) => {
         const product = await productService.getOne(req.params.productId).lean();
         const isOwner = req.user?._id == product.owner._id;
         const userId = new mongoose.Types.ObjectId(req.user?._id);
-        const hasPrefered = product.preferList.some(preferedUser => preferedUser._id.equals(userId));
+
+        const hasPrefered = await productService.hasPrefered(userId, req.params.productId);
 
         res.render('product/details', { product, isOwner, hasPrefered });
     }
     catch (error) {
-        console.log('Error getting DEVICE!')
+        res.render('product/details', { error: getErrorMsg(error) });
     }
 });
 
 router.get('/:productId/prefer', async (req, res) => {
     const productId = req.params.productId;
     try {
-        const product = await productService.getOne(productId)
+        const userId = new mongoose.Types.ObjectId(req.user?._id);
 
-        const userAlreadyPrefered = product.preferList.some(preferedUser => preferedUser._id.equals(req.user._id));
+        const userAlreadyPrefered = await productService.hasPrefered(userId, req.params.productId);
 
         if (userAlreadyPrefered) {
             throw new Error('You already prefered this device');
@@ -68,7 +69,62 @@ router.get('/:productId/prefer', async (req, res) => {
         res.redirect(`/products/${productId}/details`)
     }
     catch (error) {
-        console.log(error.message)
+        res.render('404', { error: getErrorMsg(error) });
+    }
+});
+
+router.get('/:productId/delete', async (req, res) => {
+    try {
+        const product = await productService.getOne(req.params.productId).lean();
+        const isOwner = req.user?._id == product.owner._id;
+
+        if (!isOwner) {
+            throw new Error('You cannot reach this page, you are not the owner of the product!');
+        }
+
+        await productService.deleteProduct(req.params.productId);
+
+        res.redirect('/');
+    }
+    catch (error) {
+        res.render('home/home', { error: getErrorMsg(error) });
+    }
+});
+
+router.get('/:productId/edit', async (req, res) => {
+    try {
+        const product = await productService.getOne(req.params.productId).lean();
+        const isOwner = req.user?._id == product.owner._id;
+
+        if (!isOwner) {
+            throw new Error('You cannot reach this page, you are not the owner of the product!');
+        }
+
+        res.render('product/edit', { product });
+    }
+    catch (error) {
+        res.render('404', { error: getErrorMsg(error) });
+    }
+});
+
+router.post('/:productId/edit', async (req, res) => {
+    try {
+        const productId = req.params.productId;
+
+        Object.values(req.body).forEach(field => {
+            if (!field) {
+                throw new Error('All fields are required!');
+            }
+        });
+
+        await productService.edit(productId, req.body);
+
+        res.redirect(`/products/${productId}/details`);
+    }
+    catch (error) {
+        const product = await productService.getOne(req.params.productId).lean();
+
+        res.render('product/edit', { product, error: getErrorMsg(error) });
     }
 });
 
