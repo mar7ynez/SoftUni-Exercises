48849fc86ae4424caf75f728eb053eb7443cbc0a
unknown <sadface_@abv.bg>
2024-10-06 23:31:04 +0300

    Add has prefered to the product service to check in db if user already prefered the product

--- JS Back-End/JS Back-End Retake Exam - 14 August 2024/Tech Store/src/services/productService.js
+++ JS Back-End/JS Back-End Retake Exam - 14 August 2024/Tech Store/src/services/productService.js
@@ -3,7 +3,19 @@ import { Device } from "../models/Device.js";
 export const create = (product) => Device.create(product);
 export const getAll = () => Device.find().populate('owner');
 export const getOne = (productId) => Device.findById(productId).populate('owner').populate('preferList');
+export const deleteProduct = (productId) => Device.findByIdAndDelete(productId);
+export const edit = (productId, editedData) => Device.findByIdAndUpdate(productId, editedData);
 
 export const preferDevice = (productId, userId) => {
     return Device.findByIdAndUpdate(productId, { $addToSet: { preferList: userId } }).populate('preferList');
+}
+
+export const hasPrefered = async (userId, productId) => {
+    const product = await Device.find({ _id: productId, preferList: { $in: [userId] } });
+
+    if (product.length) {
+        return true;
+    }
+
+    return false;
 }
