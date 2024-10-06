98469acaf3ab68e555508d566d37767c1227415c
unknown <sadface_@abv.bg>
2024-10-06 23:28:22 +0300

    Handle errors in auth controller

--- JS Back-End/JS Back-End Retake Exam - 14 August 2024/Tech Store/src/controllers/authController.js
+++ JS Back-End/JS Back-End Retake Exam - 14 August 2024/Tech Store/src/controllers/authController.js
@@ -1,10 +1,20 @@
 import express from "express";
 import * as authService from "../services/authService.js"
+import { getErrorMsg } from "../utils/errorUtils.js";
 
 const router = express.Router();
 
 router.get('/register', (req, res) => {
-    res.render('auth/register');
+    try {
+        if (req.user) {
+            throw new Error('You cannot reach this page while logged in!')
+        }
+
+        res.render('auth/register');
+    }
+    catch (error) {
+        res.render('404', { error: getErrorMsg(error) });
+    }
 });
 
 router.post('/register', async (req, res) => {
@@ -16,12 +26,21 @@ router.post('/register', async (req, res) => {
         res.redirect('/');
     }
     catch (error) {
-        console.log('Register Error\n', error);
+        res.render('auth/register', { error: getErrorMsg(error) });
     }
 });
 
 router.get('/login', (req, res) => {
-    res.render('auth/login');
+    try {
+        if (req.user) {
+            throw new Error('You cannot reach this page while logged in!')
+        }
+
+        res.render('auth/login');
+    }
+    catch (error) {
+        res.render('404', { error: getErrorMsg(error) });
+    }
 });
 
 router.post('/login', async (req, res) => {
@@ -34,13 +53,23 @@ router.post('/login', async (req, res) => {
         res.redirect('/');
     }
     catch (error) {
-        console.log('Login Error\n', error);
+        res.render('auth/login', { error: getErrorMsg(error) });
     }
 });
 
 router.get('/logout', (req, res) => {
-    res.clearCookie('auth');
-    res.redirect('/');
+    try {
+        if (!req.user) {
+            throw new Error('You\'ve been already logged out!');
+        }
+
+        res.clearCookie('auth');
+        res.redirect('/');
+    }
+    catch (error) {
+        res.render('404', { error: getErrorMsg(error) });
+    }
 });
+
 
 export { router };
