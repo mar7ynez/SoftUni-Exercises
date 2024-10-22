f11fca7c51929baf6ded82a4e5a5867ea510daf0
unknown <sadface_@abv.bg>
2024-10-22 23:44:58 +0300

    Add error global handler

--- JS Back-End/JS Back-End Regular Exam - 18 Feb 2024/src/router.js
+++ JS Back-End/JS Back-End Regular Exam - 18 Feb 2024/src/router.js
@@ -5,6 +5,7 @@ const router = Router();
 import { homeController } from "./controllers/homeController.js";
 import { authController } from "./controllers/authController.js";
 import { stoneController } from "./controllers/stoneController.js";
+import { getErrorMsg } from "./utils/getErrorMessage.js";
 
 router.use(homeController);
 router.use('/user', authController);
@@ -12,6 +13,12 @@ router.use('/stones', stoneController);
 
 router.get('*', (req, res) => {
     res.render('404');
+});
+
+router.use((error, req, res, next) => {
+    if (error) {
+        res.render('404', { error: getErrorMsg(error) });
+    }
 });
 
 export { router };
